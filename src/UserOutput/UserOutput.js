9d9e3e9393d55c3bd42f5999d3e8c55d9803d73d
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-22 18:53:42 +0300

    Initial commit

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const userOutput = (props) =>{
+
+    return (
+        <div>
+            <p>Username: {props.userName}</p>
+            <p>I hope I'll be rewritten</p>
+        </div>
+    )
+}
+
+export default userOutput;
